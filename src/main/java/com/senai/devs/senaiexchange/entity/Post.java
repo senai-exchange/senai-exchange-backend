2fcf7e265346dcf7cca8a6de394dcaab246a3a04
sarahsj-dot <sarah.sdejesusp@gmail.com>
2025-12-01 10:59:49 -0300

    feat: implements post entity

---
+++ src/main/java/com/senai/devs/senaiexchange/entity/Post.java
@@ -0,0 +1,92 @@
+package com.senai.devs.senaiexchange.entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+public class Post{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private int author_id;
+    @Column(nullable = false, length = 200)
+    private String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private boolean status = false;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date published_at;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private String created_at = "";
+
+    public Post() {
+
+    }
+
+    public Post(String title, String content, String created_at, Date published_at) {
+        this.title = title;
+        this.content = content;
+        this.created_at = created_at;
+        this.published_at = published_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getPublished_at() {
+        return published_at;
+    }
+
+    public void setPublished_at(Date published_at) {
+        this.published_at = published_at;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+}
