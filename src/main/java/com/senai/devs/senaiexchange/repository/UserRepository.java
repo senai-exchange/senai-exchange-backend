50f96c740a26c851cd1ec42b02d5621275907e46
th-cs <202718515+th-cs@users.noreply.github.com>
2025-12-02 10:34:03 -0300

    feat: add UserRepository

---
+++ src/main/java/com/senai/devs/senaiexchange/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.senai.devs.senaiexchange.repository;
+
+import com.senai.devs.senaiexchange.entity.User;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	User findByUsername(String username);
+}
