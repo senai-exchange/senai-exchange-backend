12c062a95ed50d1f136f388fd8334c278b4c268e
th-cs <202718515+th-cs@users.noreply.github.com>
2025-12-03 04:42:31 -0300

    feat: add UserResponse

---
+++ src/main/java/com/senai/devs/senaiexchange/dto/response/UserResponse.java
@@ -0,0 +1,29 @@
+package com.senai.devs.senaiexchange.dto.response;
+
+public class UserResponse {
+
+	// Attributes
+	private String username;
+	private String display_name;
+	private String avatar_url;
+
+	// Constructor
+	public UserResponse(String username, String display_name, String avatar_url) {
+		this.username = username;
+		this.display_name = display_name;
+		this.avatar_url = avatar_url;
+	}
+
+	// Getters
+	public String getUsername() {
+		return username;
+	}
+
+	public String getDisplay_name() {
+		return display_name;
+	}
+
+	public String getAvatar_url() {
+		return avatar_url;
+	}
+}
