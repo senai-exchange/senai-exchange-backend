19e9f38bf8d16fa475d88739a5d1cadd1c4b25b7
th-cs <202718515+th-cs@users.noreply.github.com>
2025-12-05 04:58:51 -0300

    fix: change constructor attributes

--- src/main/java/com/senai/devs/senaiexchange/dto/response/CommentResponse.java
+++ src/main/java/com/senai/devs/senaiexchange/dto/response/CommentResponse.java
@@ -7,7 +7,9 @@ import java.time.LocalDateTime;
 public class CommentResponse {
 
 	// Attributes
+	private String username;
 	private int post_id;
+	private int author_id;
 	private String content;
 	private LocalDateTime created_at;
 
@@ -16,14 +18,24 @@ public class CommentResponse {
 	}
 
 	public CommentResponse(Comment comment) {
-		this.post_id = comment.getPost_id();
+		this.username = comment.getUser().getUsername();
+		this.post_id = comment.getPost().getId();
+		this.author_id = comment.getUser().getId();
 		this.content = comment.getContent();
 		this.created_at = comment.getCreated_at();
 	}
 
 	// Getters
+	public String getUsername() {
+		return username;
+	}
+
 	public int getPost_id() {
 		return post_id;
+	}
+
+	public int getAuthor_id() {
+		return author_id;
 	}
 
 	public String getContent() {
